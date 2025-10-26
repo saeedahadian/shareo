42c5bb424d89f54ecf70ea71841a4bb1cbb39be1
Saeed Ahadian <saeed.ahadian96@gmail.com>
2025-10-26 19:34:25 +0330

    feat: use raw terminal to manage user input.

--- go.mod
+++ go.mod
@@ -3,7 +3,12 @@ module github.com/saeedahadian/shareo
 go 1.25.3
 
 require (
+	github.com/spf13/cobra v1.10.1
+	golang.org/x/term v0.36.0
+)
+
+require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.10.1 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
+	golang.org/x/sys v0.37.0 // indirect
 )
--- internal/client/client.go
+++ internal/client/client.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net"
 	"os"
+
+	"golang.org/x/term"
 )
 
 var (
@@ -13,6 +15,24 @@ var (
 
 const (
 	inputPrompt = "\033[32m> \033[0m"
+	cmdPrompt   = "\033[36m/ \033[0m"
+	serverPrompt = "\033[33m< \033[0m"
+)
+
+type inputMode int
+
+const (
+	normalMode inputMode = iota
+	commandMode
+	serverMode
+)
+
+const (
+	ctrlC      byte = 3
+	ctrlU      byte = 21
+	enter      byte = 13
+	backspace1 byte = 127
+	backspace2 byte = 8
 )
 
 func Connect(port int) {
@@ -34,15 +54,78 @@ func Connect(port int) {
 
 func handleUserInput(msgChan chan string) {
 	defer close(msgChan)
-	fmt.Print(inputPrompt)
-	inputScanner := bufio.NewScanner(os.Stdin)
-	for inputScanner.Scan() {
-		msgChan <- inputScanner.Text()
-	}
 
-	if err := inputScanner.Err(); err != nil {
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
+	}
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
+
+	mode := normalMode
+	buffer := []rune{}
+
+	showPrompt(mode)
+
+	buf := make([]byte, 1)
+	for {
+		n, err := os.Stdin.Read(buf)
+		if err != nil || n == 0 {
+			break
+		}
+		char := buf[0]
+
+		switch char {
+		case ctrlC:
+			fmt.Println("\r")
+			return
+		case ctrlU:
+			buffer = []rune{}
+			clearLine()
+			showPrompt(mode)
+		case enter:
+			fmt.Print("\r\n")
+			if len(buffer) > 0 {
+				line := string(buffer)
+				if mode == commandMode {
+					line = "/" + line
+				}
+				msgChan <- line
+				buffer = []rune{}
+			}
+			mode = normalMode
+			// showPrompt(mode)
+		case backspace1, backspace2:
+			if len(buffer) > 0 {
+				buffer = buffer[:len(buffer)-1]
+				fmt.Printf("\b \b")
+			} else if mode == commandMode {
+				mode = normalMode
+				clearLine()
+				showPrompt(mode)
+			}
+		case '/':
+			if len(buffer) == 0 && mode == normalMode {
+				mode = commandMode
+				clearLine()
+				showPrompt(mode)
+			} else {
+				buffer = append(buffer, rune(char))
+				fmt.Printf("%c", char)
+			}
+		case '>':
+			if len(buffer) == 0 && mode == commandMode {
+				mode = normalMode
+				clearLine()
+				showPrompt(mode)
+			} else {
+				buffer = append(buffer, rune(char))
+				fmt.Printf("%c", char)
+			}
+		default:
+			buffer = append(buffer, rune(char))
+			fmt.Printf("%c", char)
+		}
 	}
 }
 
@@ -50,12 +133,28 @@ func handleServerResp(conn net.Conn) {
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
 		text := scanner.Text()
-		fmt.Printf("Server: %s\n", text)
-		fmt.Print(inputPrompt)
+		showPrompt(serverMode)
+		fmt.Printf("%s\r\n", text)
+		showPrompt(normalMode)
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+func showPrompt(mode inputMode) {
+	switch mode {
+	case normalMode:
+		fmt.Print(inputPrompt)
+	case commandMode:
+		fmt.Print(cmdPrompt)
+	case serverMode:
+		fmt.Print(serverPrompt)
+	}
+}
+
+func clearLine() {
+	fmt.Print("\r\033[K")
 }
